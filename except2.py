6aff1164702b8a59ffe5a92d4ddcc9b05738b910
iamaparnad <94278699+iamaparnad@users.noreply.github.com>
2022-03-06 20:31:18 +0530

    Add files via upload

---
+++ except2.py
@@ -0,0 +1,10 @@
+
+while True:
+
+    try:
+         n=int(input("ENTER ANY DIGIT "))
+         print("entered number is",n)
+         break
+    
+    except: 
+        print("PLEASE ENTER ANY INTEGER VALUE....")
