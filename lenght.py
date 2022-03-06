e9c41379f55d29af9d096ec3a8e8662b85ba0b0a
iamaparnad <94278699+iamaparnad@users.noreply.github.com>
2022-03-06 10:53:51 +0530

    Add files via upload

---
+++ lenght.py
@@ -0,0 +1,7 @@
+int1=[input("ENTER FIRST SET OF INTEGERS :")]
+int2=[input("ENTER SECOND SET OF INTEGERS :")]
+if(len(int1)==len(int2)):
+        print("BOTH LIST ARE OF SAME LENGTH")
+else:
+        print("LENGTH OF BOTH LIST ARE DIFFERENT")
+            
